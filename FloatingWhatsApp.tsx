f224409ebcd50966f7d730d52c7bac524c03b916
Nexora Studio <pcismails711@gmail.com>
2026-09-24 20:37:25 +0500

    Update FloatingWhatsApp.tsx

--- FloatingWhatsApp.tsx
+++ FloatingWhatsApp.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { MessageCircle } from 'lucide-react';
-import { getWhatsAppUrl } from '../data/menuData';
+import { getWhatsAppUrl } from './menuData';
 
 export const FloatingWhatsApp: React.FC = () => {
   return (
